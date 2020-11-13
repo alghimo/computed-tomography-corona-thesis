620b2d400d674f14e840101efff00de7f082e39d
Albert Gimenez Morales <albert.gimenez.morales@gmail.com>
2020-11-13 18:07:23 +0100

    Update validation split to be by patient id as well

--- notebooks/nb_init.py
+++ notebooks/nb_init.py
@@ -1,16 +1,22 @@
-
 from pathlib import Path
 import pandas as pd
 
 base_dir = Path.cwd().parent
 config_dir = base_dir / "config"
 data_dir = base_dir / "data"
-input_dir = data_dir / "COVID19"
+models_dir = base_dir / "models"
+logs_dir = base_dir / "logs"
+images_input_dir = data_dir / "COVID19"
 preprocessed_dir = data_dir / "preprocessed"
 output_dir = data_dir / "output"
 
-metadata_file = input_dir / "metadata.csv"
-labels_file = input_dir / "unzip_filenames.csv"
+# Directories used to train the CNN (image by image) 
+cnn_data_dir = data_dir / "modelling" / "cnn"
+cnn_train_dir = cnn_data_dir / "train"
+cnn_test_dir = cnn_data_dir / "test"
+
+metadata_file = images_input_dir / "metadata.csv"
+labels_file = images_input_dir / "unzip_filenames.csv"
 preprocessed_labels_file = preprocessed_dir / "labels.parquet"
 
 from pyspark.sql import SparkSession
@@ -21,4 +27,13 @@ spark = SparkSession.builder.getOrCreate()
 config_file = config_dir / "tfg.conf"
 
 from pyhocon import ConfigFactory
-config = ConfigFactory.parse_file(config_file).tfg
+config = ConfigFactory.parse_file(config_file)
+
+import sys
+
+if str(base_dir / "src") not in sys.path:
+    sys.path.append(str(base_dir / "src"))
+
+%load_ext autoreload
+
+%autoreload 2
